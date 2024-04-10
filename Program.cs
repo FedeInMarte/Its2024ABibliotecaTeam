9a0e280202e104685af6dfa642ca582db1f88793
Federico Martelloni <federico.martelloni@agic.cloud>
2024-04-10 17:31:15 +0200

    cancellata guid su classe utente

--- Program.cs
+++ Program.cs
@@ -34,7 +34,6 @@
             this.name = name;
             this.surname = surname;
             this.annoIscrizione = annoIscrizione;
-            this.id = Guid.NewGuid().ToString();
         }
 
         internal string Denominazione()
