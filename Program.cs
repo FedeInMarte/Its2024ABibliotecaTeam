8384155e16a7a9ecfe763d366a18b781b65996fe
francesco-renghi <allievo_renghi.f@itsumbria.it>
2024-04-12 11:51:24 +0200

    aggiunte proprietà classe Libro

--- Program.cs
+++ Program.cs
@@ -4,79 +4,93 @@
     {
         static void Main(string[] args)
         {
-            Utente Federico = new Utente("000001", "Federico", "Martelloni", 2024);
-            Utente Pino = new Utente("000002", "Pino", "Abetoni", 2024);
+            Utente f = new Utente("001", "Francesco", "Renghi", 2024);
 
-            Libro It = new Libro("001", "It","Stefano Re");
-            It.Prestito(Federico);
-            It.Prestito(Pino);
+            Utente p = new Utente("002", "Pino", "Abetone", 2023);
+
+            Libro It = new Libro("001", "It", "Stefano Re");
+
+            It.Prestito(f);
+            It.Prestito(p);
             It.Prestito(null);
 
             It.Restituzione();
-            It.Prestito(Pino);
-
-            Console.WriteLine("Hello, World!");
+            It.Prestito(p);
+            
         }
     }
 
-    internal class Utente {
-        private string id;
-        private string name;
-        private string surname;
-        private int annoIscrizione;
-        private int varInutile;
+    internal class Utente
+    {
+        private object get;
 
-        internal Utente (string id, 
-            string name, 
-            string surname, 
-            int annoIscrizione)
+        internal string Id { get; set; }
+        internal string Name { get; set; }
+        internal string Surname { get; set; }
+        internal int AnnoIscrizione { get; set; }
+        
+
+        internal Utente(string id, string name, string surname, int annoIscrizione)
         {
-            this.id = id;
-            this.name = name;
-            this.surname = surname;
-            this.annoIscrizione = annoIscrizione;
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.AnnoIscrizione = annoIscrizione;
         }
 
-        internal string Denominazione()
-        {
-            return $"{this.name} {this.surname}"; 
-            //this.name + " " + this.surname;
+
+        internal string Descrizione{
+            get{ return $"{this.Name} {this.Surname}"; }
         }
+
     }
 
-    internal class Libro {
-        private string id;
-        private string title;
-        private string author;
-        private Utente utente;
-    
+
+
+
+
+
+
+    internal class Libro
+    {
+        internal string Id { get; set; }
+        internal string Title { get; set; }
+        internal string Author { get; set; }
+
+        internal Utente Utente { get; set; }
+        internal string Descrizione
+        {
+            get
+            {
+                return $"{this.Title} di {this.Author}";
+            }
+
+        }
+
         internal Libro(string id, string title, string author)
         {
-            this.id = id;
-            this.title = title;
-            this.author = author;
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
         }
 
-        internal string Descrizione()
+
+        internal void Prestito(Utente? utente)
         {
-            return $"{title} di {author}";
-        }
-        internal void Prestito(Utente? utente) {
 
-            //var locale e non d'istana
-            if (utente == null) { } 
+            //var locale e non d'istanza
+            if (utente == null) { }
 
 
-            if (this.utente == null)
-                this.utente = utente;
+            if (this.Utente == null)
+                this.Utente = utente;
             else
                 Console.WriteLine("Libro già in prestito");
         }
         internal void Restituzione()
         {
-            Console.WriteLine(
-                $"Libro {title} restituito da {utente.Denominazione()}");
-            this.utente = null;
+            Console.WriteLine($"Libro {this.Title} restituito da {this.Utente.Descrizione}");
+            this.Utente = null;
         }
 
     }
