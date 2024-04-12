b128d4487504308b08cd9838aae84520c1b157cc
GF1630 <F.gammaidoni@outlook.com>
2024-04-12 12:11:56 +0200

    Modifiche: -> Cambio delle variabili delle classi Utente in Libro in proprietà con relativi getter e setter. -> Cambio dei metodi Denominazione() e Descrizione() in proprietà della classe. -> Refactoring dei metodi Prestito() e Restituzione().

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Its2024ABibliotecaTeam
+﻿using System.Dynamic;
+
+namespace Its2024ABibliotecaTeam
 {
     internal class Program
     {
@@ -6,8 +8,8 @@
         {
             Utente Federico = new Utente("000001", "Federico", "Martelloni", 2024);
             Utente Pino = new Utente("000002", "Pino", "Abetoni", 2024);
-
             Libro It = new Libro("001", "It","Stefano Re");
+            
             It.Prestito(Federico);
             It.Prestito(Pino);
             It.Prestito(null);
@@ -15,70 +17,75 @@
             It.Restituzione();
             It.Prestito(Pino);
 
-            Console.WriteLine("Hello, World!");
+            Console.Write(Pino.Denominazione);
         }
     }
 
     internal class Utente {
-        private string id;
-        private string name;
-        private string surname;
-        private int annoIscrizione;
-        private int varInutile;
+        internal string id {  get; set; }
+        internal string name { get; set; }
+        internal string surname { get; set; }
+        internal int annoIscrizione { get; set; }
+        public string Denominazione { get { return $"{this.name} {this.surname}"; } }
 
-        internal Utente (string id, 
-            string name, 
-            string surname, 
-            int annoIscrizione)
+        /*Costruttore della classe Utente*/
+        internal Utente (string id, string name, string surname, int annoIscrizione)               
         {
             this.id = id;
             this.name = name;
             this.surname = surname;
             this.annoIscrizione = annoIscrizione;
         }
-
-        internal string Denominazione()
-        {
-            return $"{this.name} {this.surname}"; 
-            //this.name + " " + this.surname;
-        }
     }
 
     internal class Libro {
-        private string id;
-        private string title;
-        private string author;
-        private Utente utente;
-    
+        internal string id {  get; set; }
+        internal string title { get; set; }
+        internal string author { get; set; }
+        internal Utente? utente { get; set; }
+        public string Descrizione { get { return $"{title} di {author}"; } }
+        
+        /*Il costruttore della classe Libro*/
         internal Libro(string id, string title, string author)
         {
             this.id = id;
             this.title = title;
             this.author = author;
-        }
-
-        internal string Descrizione()
-        {
-            return $"{title} di {author}";
-        }
-        internal void Prestito(Utente? utente) {
-
-            //var locale e non d'istana
-            if (utente == null) { } 
-
-
-            if (this.utente == null)
-                this.utente = utente;
-            else
-                Console.WriteLine("Libro già in prestito");
-        }
-        internal void Restituzione()
-        {
-            Console.WriteLine(
-                $"Libro {title} restituito da {utente.Denominazione()}");
             this.utente = null;
         }
 
-    }
+        internal void Prestito(Utente? utente)
+        {
 
+            if (utente != null)
+            {
+                if (this.utente == null)
+                {
+                    this.utente = utente == null ? null : utente;
+                    Console.WriteLine($"{this.Descrizione} > {utente?.Denominazione}");
+                }
+                else
+                {
+                    Console.WriteLine("Err. Already on loan");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Err. Arguments");
+
+            }
+        }
+        internal void Restituzione()
+        {
+            if (this.utente == null)
+            {
+                Console.WriteLine("Err. Not on loan");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Descrizione} < {this.utente.Denominazione}");
+                this.utente = null;
+            }
+        }
+    }
 }
