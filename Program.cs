d8c411d7ac898786f7cd6b1b44bb0ce34150665f
AlessioFioriti <alessio.fioriti@gmail.com>
2024-04-12 11:57:51 +0200

    Aggiunte proprietà classe libro

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Utente Federico = new Utente("000001", "Federico", "Martelloni", 2024);
             Utente Pino = new Utente("000002", "Pino", "Abetoni", 2024);
 
+
             Libro It = new Libro("001", "It","Stefano Re");
             It.Prestito(Federico);
             It.Prestito(Pino);
@@ -14,69 +15,68 @@
 
             It.Restituzione();
             It.Prestito(Pino);
-
-            Console.WriteLine("Hello, World!");
         }
     }
 
     internal class Utente {
-        private string id;
-        private string name;
-        private string surname;
-        private int annoIscrizione;
-        private int varInutile;
-
+        internal string Id { get; set; }
+        internal string Name {  get; set; }
+        internal string Surname { get; set; }
+        internal int AnnoIscrizione { get; set; }
+        internal string Denominazione
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+                //this.name + " " + this.surname;
+            }
+        }
         internal Utente (string id, 
             string name, 
             string surname, 
             int annoIscrizione)
         {
-            this.id = id;
-            this.name = name;
-            this.surname = surname;
-            this.annoIscrizione = annoIscrizione;
-        }
-
-        internal string Denominazione()
-        {
-            return $"{this.name} {this.surname}"; 
-            //this.name + " " + this.surname;
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.AnnoIscrizione = annoIscrizione;
         }
     }
 
     internal class Libro {
-        private string id;
-        private string title;
-        private string author;
-        private Utente utente;
-    
+        internal string Id { get; set;}
+        internal string Title {  get; set; }
+        internal string Author { get; set; }
+        internal Utente Utente { get; set; }
+        internal string Descrizione
+        {
+            get
+            {
+                return $"{Title} di {Author}";
+            }
+        }
         internal Libro(string id, string title, string author)
         {
-            this.id = id;
-            this.title = title;
-            this.author = author;
-        }
-
-        internal string Descrizione()
-        {
-            return $"{title} di {author}";
-        }
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
+        }   
         internal void Prestito(Utente? utente) {
 
             //var locale e non d'istana
             if (utente == null) { } 
 
 
-            if (this.utente == null)
-                this.utente = utente;
+            if (this.Utente == null)
+                this.Utente = utente;
             else
                 Console.WriteLine("Libro già in prestito");
         }
         internal void Restituzione()
         {
             Console.WriteLine(
-                $"Libro {title} restituito da {utente.Denominazione()}");
-            this.utente = null;
+                $"Libro {Title} restituito da {Utente.Denominazione}");
+            this.Utente = null;
         }
 
     }
