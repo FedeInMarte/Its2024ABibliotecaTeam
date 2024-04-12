35ca7c431c26afde5579cbec318b159596dae94a
francesco-renghi <allievo_renghi.f@itsumbria.it>
2024-04-12 12:46:29 +0200

    Caricamento file personale

--- Program.cs
+++ Program.cs
@@ -1,46 +1,53 @@
 ﻿namespace Its2024ABibliotecaTeam
 {
-    internal class Program
+    internal class Library
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Utente f = new Utente("001", "Francesco", "Renghi", 2024);
+            Console.Clear();
 
-            Utente p = new Utente("002", "Pino", "Abetone", 2023);
 
-            Libro It = new Libro("001", "It", "Stefano Re");
+            User user1 = new User("0001", "Francesco", "Renghi", 2019);
+            User user2 = new User("0002", "Jamal", "Benkhalek", 2023);
 
-            It.Prestito(f);
-            It.Prestito(p);
-            It.Prestito(null);
-
-            It.Restituzione();
-            It.Prestito(p);
-            
+            Book book1 = new Book("0001", "La Divina Commedia", "Dante Alighieri");
+            Book book2 = new Book("0002", "Dune", "Frank Herbert");
+            book1.Loan(user1);
+            Console.ReadKey();
+            book1.Loan(user2);
+            Console.ReadKey();
+            book1.Return();
+            Console.ReadKey();
+            book2.Return();
+            Console.ReadKey();
+            book1.Loan(user2);
+            Console.ReadKey();
         }
     }
 
-    internal class Utente
+    internal class User
     {
-        private object get;
-
         internal string Id { get; set; }
         internal string Name { get; set; }
-        internal string Surname { get; set; }
-        internal int AnnoIscrizione { get; set; }
-        
+        internal string LastName { get; set; }
+        internal int SubYear { get; set; }
 
-        internal Utente(string id, string name, string surname, int annoIscrizione)
+
+        internal User(string id, string name, string lastName, int subYear)
         {
             this.Id = id;
             this.Name = name;
-            this.Surname = surname;
-            this.AnnoIscrizione = annoIscrizione;
+            this.LastName = lastName;
+            this.SubYear = subYear;
         }
 
 
-        internal string Descrizione{
-            get{ return $"{this.Name} {this.Surname}"; }
+        internal string Denominazione
+        {
+            get
+            {
+                return $"{this.Name} {this.LastName}";
+            }
         }
 
     }
@@ -51,13 +58,21 @@
 
 
 
-    internal class Libro
+    internal class Book
     {
         internal string Id { get; set; }
         internal string Title { get; set; }
         internal string Author { get; set; }
+        internal User? User { get; set; }
 
-        internal Utente Utente { get; set; }
+
+        internal Book(string id, string title, string author)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
+        }
+
         internal string Descrizione
         {
             get
@@ -67,30 +82,32 @@
 
         }
 
-        internal Libro(string id, string title, string author)
-        {
-            this.Id = id;
-            this.Title = title;
-            this.Author = author;
-        }
-
-
-        internal void Prestito(Utente? utente)
+        internal void Loan(User user)
         {
 
-            //var locale e non d'istanza
-            if (utente == null) { }
-
-
-            if (this.Utente == null)
-                this.Utente = utente;
+            if (this.User != null)
+            {
+                Console.WriteLine($"{user.Denominazione} non puoi prendere {this.Descrizione}, è già in prestito da {this.User.Denominazione}");
+            }
             else
-                Console.WriteLine("Libro già in prestito");
+            {
+                this.User = user;
+                Console.WriteLine($"{this.Descrizione}: prestito effettuato a {user.Denominazione}");
+            }
         }
-        internal void Restituzione()
+
+        internal void Return()
         {
-            Console.WriteLine($"Libro {this.Title} restituito da {this.Utente.Descrizione}");
-            this.Utente = null;
+
+            if (this.User != null)
+            {
+                Console.WriteLine($"{this.User.Denominazione} ha restituito {this.Descrizione}");
+                this.User = null;
+            }
+            else
+            {
+                Console.WriteLine($"{this.Descrizione} è disponibile per il Prestito!");
+            }
         }
 
     }
