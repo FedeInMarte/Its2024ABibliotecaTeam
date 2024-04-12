3ac6f35094ce009cf15fb6073fbd11def4239bf2
Elisa Mariotti <allievo_Mariotti.E@itsumbria.it>
2024-04-12 11:46:02 +0200

    abbiamo modificato le variabili

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Utente Federico = new Utente("000001", "Federico", "Martelloni", 2024);
-            Utente Pino = new Utente("000002", "Pino", "Abetoni", 2024);
+            Utente Federico = new Utente ("000001","Federico","Martelloni",2024);
+            
+            Utente Pino = new Utente ("000002","Pino","Abetoni", 2024 );
+
 
             Libro It = new Libro("001", "It","Stefano Re");
             It.Prestito(Federico);
@@ -20,65 +22,60 @@
     }
 
     internal class Utente {
-        private string id;
-        private string name;
-        private string surname;
-        private int annoIscrizione;
-        private int varInutile;
+        string Id { get; set;}
+        string Name { get; set; }
+        string Surname { get; set; }
+        int AnnoIscrizione { get; set; }
+        internal string Denominazione {
+            get { return $"{Name}{Surname}";}
+                     } 
+    
 
         internal Utente (string id, 
             string name, 
             string surname, 
             int annoIscrizione)
         {
-            this.id = id;
-            this.name = name;
-            this.surname = surname;
-            this.annoIscrizione = annoIscrizione;
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.AnnoIscrizione = annoIscrizione;
         }
 
-        internal string Denominazione()
-        {
-            return $"{this.name} {this.surname}"; 
-            //this.name + " " + this.surname;
-        }
     }
 
     internal class Libro {
-        private string id;
-        private string title;
-        private string author;
-        private Utente utente;
+        string Id { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        Utente Utente { get; set; }
+    string Descrizione { get { return $"{Title} di {Author}"; }}
     
         internal Libro(string id, string title, string author)
         {
-            this.id = id;
-            this.title = title;
-            this.author = author;
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
         }
 
-        internal string Descrizione()
-        {
-            return $"{title} di {author}";
-        }
+
         internal void Prestito(Utente? utente) {
 
             //var locale e non d'istana
             if (utente == null) { } 
 
 
-            if (this.utente == null)
-                this.utente = utente;
+            if (this.Utente == null)
+                this.Utente = utente;
             else
                 Console.WriteLine("Libro già in prestito");
         }
         internal void Restituzione()
         {
             Console.WriteLine(
-                $"Libro {title} restituito da {utente.Denominazione()}");
-            this.utente = null;
+                $"Libro {Title} restituito da {Utente.Denominazione}");
+            this.Utente = null;
         }
 
     }
 
-}
