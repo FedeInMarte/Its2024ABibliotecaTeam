e81cbf6cdb351f5bbeb8faeae7049de2a15b8a8d
martinabruni <mbruni02@gmail.com>
2024-04-15 11:17:03 +0200

    Class Organizzazione created

--- Program.cs
+++ Program.cs
@@ -28,7 +28,20 @@ namespace Its2024ABibliotecaTeam
             }
         }
     }
-    
+    internal class Organizzazione : IUtente
+    {
+        internal string RagioneSociale { get; set; }
+        public string Id { get; set; }
+        public int AnnoIscrizione { get; set; }
+        public string Denominazione
+        {
+            get
+            {
+                return $"{Id} {RagioneSociale}";
+            }
+        }
+    }
+
 }
 
     internal class Libro {
